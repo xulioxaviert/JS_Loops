54a37f51ddd37114d1a76be54b190f835e7a2b7f
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-03-02 14:21:41 +0100

    Iteración #1: Usa includes

---
+++ includes.js
@@ -0,0 +1,21 @@
+//Iteración #1: Usa includes
+
+// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .includes de javascript.
+
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
+
+
+
+for (let i = 0; i < products.length; i++) {
+    if (products[i].includes('Camiseta')) {
+        console.log(products[i]);
+    }
+    
+}
